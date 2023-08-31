c80e4f6305ccba214da2d286204b7cad4b019bc0
Daniel Sheppard <dans@dansheps.com>
2023-08-30 22:24:57 -0500

    Update for NetBox 3.6

--- netbox_zabbix/__init__.py
+++ netbox_zabbix/__init__.py
@@ -12,7 +12,7 @@ class ZabbixPlugin(PluginConfig):
     author_email = metadata.get('Author-email')
     base_url = 'zabbix'
     min_version = '3.2.0'
-    max_version = '3.5.99'
+    max_version = '3.6.99'
     required_settings = [
         'username',
         'password',
